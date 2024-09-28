893bbd15ab8c56e1a1055a7ade4f8431ac3a34fa
Aharva Karale <atharvakarale2604@gmail.com>
2024-09-28 18:57:38 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class Calculator {
   public static String Grade(double percentage) {
         if (percentage >= 90) {
@@ -19,36 +18,25 @@ public class Calculator {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-          int numSubjects;
-        // Input number of subjects
-        System.out.print("Enter the number of subjects: ");
-        numSubjects = scanner.nextInt();
-
-        // Array to store marks of each subject
-        int[] marks = new int[numSubjects];
-
-        // Taking input for marks of each subject
-        for (int i = 0; i < numSubjects; i++) {
+          int num;
+     System.out.print("Enter the number of subjects: ");
+        num = scanner.nextInt();
+     int[] marks = new int[num];
+    
+      for (int i = 0; i < num; i++) {
             System.out.print("Enter marks for subject " + (i + 1) + " (out of 100): ");
             marks[i] = scanner.nextInt();
         }
-
-        // Calculate total marks
         int totalMarks = 0;
         for (int mark : marks) {
             totalMarks += mark;
         }
-
-        // Calculate average percentage
-        double averagePercentage = (double) totalMarks / numSubjects;
-
-        // Calculate grade
-        String grade = Grade(averagePercentage);
-
-        // Display results
-        System.out.println("\n--- Results ---");
+      
+      double average = (double) totalMarks / num;
+      String grade = Grade(averagePercentage);
+       System.out.println("\n--- Results ---");
         System.out.println("Total Marks: " + totalMarks);
-        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Average Percentage: " + average + "%");
         System.out.println("Grade: " + grade);
     }
 }
